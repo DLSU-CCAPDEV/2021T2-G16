59a6e9825a68cb003eb52a64190cbf51c24adb73
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-22 12:58:45 +0800

    Form handling

--- collaborative/src/components/Form/Form.js
+++ collaborative/src/components/Form/Form.js
@@ -2,66 +2,81 @@ import React, { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import styles from "./Form.module.css";
 
-var linkTo = "";
-var initialFormData = {};
-
-const renderChildren = (children) => {
-  return children.map((item) => {
-    if (item.props.to !== undefined) {
-      return (
-        <div
-          className={
-            item.props.flex_end
-              ? styles.flex_end
-              : null || item.props.flex_center
-              ? styles.flex_center
-              : null
-          }
-          onClick={clearForm}
-        >
-          {item}
-        </div>
-      );
-    } else {
-      switch (item.type) {
-        //    Get to root elements
-        case "div":
-          return (
-            <div className={item.props.column ? styles.column : styles.row}>
-              {renderChildren(item.props.children)}
-            </div>
-          );
-        case "input":
-          if (item.props.type === "submit") {
-            linkTo = item.props.linkTo;
-          } else {
-            initialFormData[item.props.id] = null;
-          }
-          return item;
-        default:
-          return item;
-      }
-    }
-  });
-};
-
 const clearForm = () => {
   document.getElementById("form").reset();
 };
 
+var linkTo = "";
+
 const Form = ({ children, width }) => {
-  const [formData, updateFormData] = useState(initialFormData);
+  const [formData, updateFormData] = useState({});
   const history = useHistory();
   const forwardToLink = useCallback(() => history.push(`/${linkTo}`), [
     history,
   ]);
 
+  const handleOnChange = (event) => {
+    //  TODO hide password
+    updateFormData({
+      ...formData,
+      [event.target.id]: event.target.value.trim(),
+    });
+  };
+
   const handleOnSubmit = (event, forwardToLink) => {
     event.preventDefault();
-    // console.log(event);
-    // console.log(event.target[0].value);
     //  TODO account exists / wrong credentials
     forwardToLink();
+  };
+
+  const renderChildren = (children) => {
+    return children.map((item) => {
+      if (item.props.to !== undefined) {
+        return (
+          <div
+            className={
+              item.props.flex_end
+                ? styles.flex_end
+                : null || item.props.flex_center
+                ? styles.flex_center
+                : null
+            }
+            onClick={clearForm}
+          >
+            {item}
+          </div>
+        );
+      } else {
+        switch (item.type) {
+          //    Get to root elements
+          case "div":
+            return (
+              <div className={item.props.column ? styles.column : styles.row}>
+                {renderChildren(item.props.children)}
+              </div>
+            );
+          case "input":
+            if (item.props.type === "submit") {
+              linkTo = item.props.linkTo;
+              return item;
+            } else {
+              let props = item.props;
+              return (
+                <input
+                  type={props.type}
+                  id={props.id}
+                  name={props.name}
+                  placeholder={props.placeholder}
+                  required={props.required}
+                  onChange={handleOnChange}
+                />
+              );
+            }
+          default:
+            return item;
+        }
+      }
+    });
   };
 
   return (
