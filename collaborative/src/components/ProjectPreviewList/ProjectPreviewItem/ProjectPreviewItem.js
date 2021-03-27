e2cfae427b949f171d25608dac18c45e8bdea673
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-27 19:16:43 +0800

    Animated onHover

--- collaborative/src/components/ProjectPreviewList/ProjectPreviewItem/ProjectPreviewItem.js
+++ collaborative/src/components/ProjectPreviewList/ProjectPreviewItem/ProjectPreviewItem.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProjectPreviewItem.module.css";
+import "./ProjectPreview.css";
 
 const ProjectPreviewItem = ({ itemProp }) => {
+  const [isHoveredOn, toggleHover] = useState(false);
+
   return (
-    <div className={styles.ProjectPreviewItem}>
+    <div
+      title={itemProp.projectName}
+      onMouseEnter={() => toggleHover(true)}
+      onMouseLeave={() => toggleHover(false)}
+      className={`${styles.ProjectPreviewItem} ${
+        isHoveredOn
+          ? styles.ProjectPreviewItem__active
+          : styles.ProjectPreviewItem__inactive
+      }`}
+    >
       <div className={styles.Picture} />
-      <span className={styles.Title} title={itemProp.projectName}>
+      <span
+        className={`${styles.Title} ${
+          isHoveredOn ? styles.Title__active : styles.Title__inactive
+        }`}
+      >
         {itemProp.projectName}
       </span>
     </div>
