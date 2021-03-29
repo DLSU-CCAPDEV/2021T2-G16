82171fbafe9ca4ac6f4719233be77f7e22d10fa9
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-29 13:09:35 +0800

    If the User is not logged in while viewing user profiles, they will be redirected to the login page

--- collaborative/src/components/UserProfilePage/UserProfilePage.js
+++ collaborative/src/components/UserProfilePage/UserProfilePage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router";
 
-const UserProfilePage = ({ userAccount }) => {
-  return <div>{"ID " + userAccount.uniqueID} </div>;
+const UserProfilePage = ({ userAccount, currentUser }) => {
+  const history = useHistory();
+  const handleNoCurrentUser = useCallback(() => history.push("/login"), [
+    history,
+  ]);
+
+  if (!currentUser) {
+    handleNoCurrentUser();
+    return null;
+  } else {
+    return (
+      <div>{`ID${userAccount.uniqueID} ${
+        currentUser.uniqueID === userAccount.uniqueID ? "YOU" : "Not you"
+      }`}</div>
+    );
+  }
 };
 
-export default connect()(UserProfilePage);
+const mapStateToProps = (state) => {
+  return { currentUser: state.currentUser };
+};
+
+export default connect(mapStateToProps)(UserProfilePage);
