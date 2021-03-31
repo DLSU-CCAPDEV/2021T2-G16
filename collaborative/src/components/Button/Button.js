cf53e94060b62519c3690ffc8d6feeb42cbfd11f
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-31 23:56:05 +0800

    Refactored Button component into a functional component

--- collaborative/src/components/Button/Button.js
+++ collaborative/src/components/Button/Button.js
@@ -1,21 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Button.module.css";
 import { Link } from "react-router-dom";
 
-class Button extends React.Component {
-  static defaultProps = {
-    backgroundColor: "white",
-    color: "black",
-    hoverColor: "gray",
-  };
+const Button = ({
+  backgroundColor = "white",
+  color = "black",
+  primary,
+  bold,
+  children,
+}) => {
+  const [isHoveredOn, toggleHover] = useState(false);
 
-  state = { hoveredOn: false };
-
-  toggleHover = () => {
-    this.setState({ hoveredOn: !this.state.hoveredOn });
-  };
-
-  renderStyle = ({ primary, color, backgroundColor, bold }) => {
+  const renderStyle = () => {
     let Color = require("color");
     let primaryColor = Color(backgroundColor);
     let darkenedPrimaryColor = primaryColor.darken(0.08);
@@ -23,44 +19,59 @@ class Button extends React.Component {
 
     if (primary) {
       style = {
-        backgroundColor: this.state.hoveredOn
-          ? darkenedPrimaryColor
-          : primaryColor,
+        backgroundColor: isHoveredOn ? darkenedPrimaryColor : primaryColor,
         color: color,
       };
     } else {
       style = {
-        backgroundColor: this.state.hoveredOn ? color : "transparent",
-        color: this.state.hoveredOn ? "black" : color,
+        backgroundColor: isHoveredOn ? color : "transparent",
+        color: isHoveredOn ? "black" : color,
       };
     }
 
-    //  Apply Border to all
     style.border = `1px solid ${
-      this.state.hoveredOn && primary ? darkenedPrimaryColor : primaryColor
+      isHoveredOn && primary ? darkenedPrimaryColor : primaryColor
     }`;
     style.fontWeight = bold ? "700" : "500";
 
     return style;
   };
 
-  render() {
-    if (this.props.Link) {
+  const renderElement = () => {
+    if (children.type === Link) {
       return (
         <Link
-          to={"/" + this.props.linkTo}
-          onMouseEnter={this.toggleHover}
-          onMouseLeave={this.toggleHover}
+          to={children.props.to}
+          onMouseEnter={() => toggleHover(true)}
+          onMouseLeave={() => toggleHover(false)}
           className={styles.Button}
-          style={this.renderStyle(this.props)}
+          style={renderStyle()}
         >
-          {this.props.children}
+          {children.props.children}
         </Link>
       );
-    } else {
-      return <a href="#">{this.props.text}</a>;
     }
-  }
-}
+  };
+
+  console.table(children);
+
+  return renderElement();
+  // return
+  // if (Link) {
+  //   return (
+  //     <Link
+  //       to={"/" + this.props.linkTo}
+  //       onMouseEnter={() => toggleHover(true)}
+  //       onMouseLeave={() => toggleHover(false)}
+  //       className={styles.Button}
+  //       style={this.renderStyle(this.props)}
+  //     >
+  //       {this.props.children}
+  //     </Link>
+  //   );
+  // } else {
+  //   return <a href="#">{this.props.text}</a>;
+  // }
+};
 
 export default Button;
