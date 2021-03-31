912d91db55117bb25b02e43272e4a7db627e65f3
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-03-31 11:28:36 +0800

    onHover check of task items

--- collaborative/src/components/TaskPreviewList/TaskPreviewItem/TaskPreviewItem.js
+++ collaborative/src/components/TaskPreviewList/TaskPreviewItem/TaskPreviewItem.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import CheckMark from "../../../assets/Checkmark.svg";
+import CheckMark__Checked from "../../../assets/Checkmark__Checked.svg";
 import PrioritySignal from "../PrioritySignal/PrioritySignal";
 import styles from "./TaskPreviewItem.module.css";
 
 const TaskPreviewItem = ({ taskProps }) => {
+  const [isHoveredOn, toggleHover] = useState(false);
+
   const renderPriority = () => {
     if (taskProps.priority === "high") {
       return <PrioritySignal high />;
@@ -16,7 +19,12 @@ const TaskPreviewItem = ({ taskProps }) => {
 
   return (
     <li className={styles.TaskPreviewItem}>
-      <img src={CheckMark} alt="Check this Task" />
+      <img
+        src={isHoveredOn ? CheckMark__Checked : CheckMark}
+        onMouseEnter={() => toggleHover(true)}
+        onMouseLeave={() => toggleHover(false)}
+        alt="Check this Task"
+      />
       <span>{taskProps.taskName}</span>
       <div className={styles.Priority}>{renderPriority()}</div>
     </li>
