5f999d5d91cf92b300cf1cd515828556ef39ea4f
MGTriad <ProgrammerNammer@users.noreply.github.com>
2021-04-01 19:12:28 +0800

    Edit modal scaffolding

--- collaborative/src/components/TaskPage/TaskPage.js
+++ collaborative/src/components/TaskPage/TaskPage.js
@@ -11,11 +11,10 @@ import { Link } from "react-router-dom";
 
 // TODO Fix misalignment styling whenever there there is no more scroll
 const TaskPage = () => {
-  const [isModalEnabled, setModalStatus] = useState(false);
+  const [isAddTaskModalEnabled, setAddTaskModalStatus] = useState(false);
+  const [isEditTaskModalEnabled, setEditTaskModalStatus] = useState(false);
 
-  const handleOnEdit = () => {
-    alert("Wishing to edit");
-  };
+  const handleOnEdit = () => {};
 
   const handleOnCreate = () => {
     alert("Created");
@@ -23,10 +22,107 @@ const TaskPage = () => {
 
   const handleOnClose = () => {};
 
+  const renderAddTaskModal = () => {
+    return (
+      <Popup
+        open={isAddTaskModalEnabled}
+        modal
+        closeOnEscape={false}
+        overlayStyle={{ backgroundColor: "rgba(29, 24, 37, 0.836)" }}
+        position="center center"
+        onClose={() => setAddTaskModalStatus(false)}
+      >
+        <Form
+          width="600px"
+          formPurpose="registration"
+          errorText="This email is already used."
+          linkTo="/login"
+        >
+          <img
+            src={CloseButton}
+            alt="Close Button"
+            handleOnClick={() => setAddTaskModalStatus(false)}
+            clickable
+            flex__end
+          />
+          <h1>Create a New Task</h1>
+          <input
+            type="text"
+            name="task-name-input"
+            id="taskNameInput"
+            placeholder="Task Name"
+            required
+            editableText
+          />
+          <span>Task Description</span>
+          <textarea
+            name="descriptionInput"
+            placeholder="My utmost important task description."
+          />
+          <div column>
+            <input type="submit" value="Create Task" />
+          </div>
+        </Form>
+      </Popup>
+    );
+  };
+
+  const renderEditTaskModal = () => {
+    return (
+      <Popup
+        open={isEditTaskModalEnabled}
+        modal
+        closeOnEscape={false}
+        overlayStyle={{ backgroundColor: "rgba(29, 24, 37, 0.836)" }}
+        position="center center"
+        onClose={() => setEditTaskModalStatus(false)}
+      >
+        <Form
+          width="600px"
+          formPurpose="registration"
+          errorText="This email is already used."
+          linkTo="/login"
+        >
+          <img
+            src={CloseButton}
+            alt="Close Button"
+            handleOnClick={() => setEditTaskModalStatus(false)}
+            clickable
+            flex__end
+          />
+          <h1>Edit Task</h1>
+          <input
+            type="text"
+            name="task-name-input"
+            id="taskNameInput"
+            placeholder="Task Name"
+            required
+            editableText
+          />
+          <span>Task Description</span>
+          <textarea
+            name="descriptionInput"
+            placeholder="My utmost important task description."
+          />
+          <div column>
+            <input type="submit" value="Create Task" />
+          </div>
+        </Form>
+      </Popup>
+    );
+  };
+
   return (
     <section className={`${styles.TaskPage}`}>
+      {renderAddTaskModal()}
+      {renderEditTaskModal()}
       <div className={`narrowPage ${styles.List}`}>
-        <TaskPreviewList primary handleOnClick={handleOnEdit} />
+        <TaskPreviewList
+          primary
+          handleOnClick={() => {
+            setEditTaskModalStatus(true);
+          }}
+        />
       </div>
       <footer className={styles.TaskPage_Footer}>
         <div className={`${styles.Footer_Content}`}>
@@ -38,50 +134,10 @@ const TaskPage = () => {
             bold
             fontSize="16"
             smooth
-            onClick={() => setModalStatus(true)}
+            onClick={() => setAddTaskModalStatus(true)}
           >
             Add Task <img src={New__White} alt="New Project" />
           </Button>
-          <Popup
-            open={isModalEnabled}
-            modal
-            closeOnEscape={false}
-            overlayStyle={{ backgroundColor: "rgba(29, 24, 37, 0.836)" }}
-            position="center center"
-            onClose={() => setModalStatus(false)}
-          >
-            <Form
-              width="600px"
-              formPurpose="registration"
-              errorText="This email is already used."
-              linkTo="/login"
-            >
-              <img
-                src={CloseButton}
-                alt="Close Button"
-                handleOnClick={() => setModalStatus(false)}
-                clickable
-                flex__end
-              />
-              <h1>Create a New Task</h1>
-              <input
-                type="text"
-                name="task-name-input"
-                id="taskNameInput"
-                placeholder="Task Name"
-                required
-                editableText
-              />
-              <span>Task Description</span>
-              <textarea
-                name="descriptionInput"
-                placeholder="My utmost important task description."
-              />
-              <div column>
-                <input type="submit" value="Create Task" />
-              </div>
-            </Form>
-          </Popup>
         </div>
       </footer>
     </section>
