6242bbb973f5f716312b8070ca541083c2c6462f
Programmernammer <ProgrammerNammer@users.noreply.github.com>
2021-04-07 23:32:33 +0800

    Sidebar transition added. Temporarily removed project and user pages

--- collaborative/src/components/App/App.js
+++ collaborative/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
+import { CSSTransition } from "react-transition-group";
 import Error404NotFoundPage from "../Error404NotFoundPage/Error404NotFoundPage";
 import HomePage from "../HomePage/HomePage";
 import LandingPage from "../LandingPage/LandingPage";
@@ -24,90 +25,34 @@ const App = ({ userDatabase, projectDatabase, currentUser }) => {
     toggleSideBar(!isSideBarOpen);
   };
 
-  const WorkRoute = ({ exact, path, headerName, Component }) => {
-    return currentUser ? (
-      <Route
-        exact={exact}
-        path={path}
-        render={(props) => (
-          <div className={styles.WorkSpace} style={{ height: "100vh" }}>
-            {isSideBarOpen ? (
-              <SideBar handleOnClick={handleOnClickToggleSideBar} />
-            ) : null}
-            <div className={styles.WorkSpace_Content}>
-              <WorkspaceNavigationBar
-                handleOnClickToggleSideBar={handleOnClickToggleSideBar}
-                headerName={headerName}
-                isSideBarOpen={isSideBarOpen}
-              />
-              <div className={styles.Content_Main}>
-                <Component props={props} />
-              </div>
+  // TODO check if this can be wired with a component rather than a function
+  const routeNavigationLayout = (path, exact, headerName, Component) => {
+    return (
+      <Route exact={exact} path={path}>
+        <div className={styles.WorkSpace} style={{ height: "100vh" }}>
+          <CSSTransition
+            in={isSideBarOpen}
+            classNames={"example"}
+            timeout={500}
+            unmountOnExit
+            onEnter={() => toggleSideBar(true)}
+            onExit={() => toggleSideBar(false)}
+          >
+            <SideBar handleOnClick={handleOnClickToggleSideBar} />
+          </CSSTransition>
+          <div className={styles.WorkSpace_Content}>
+            <WorkspaceNavigationBar
+              handleOnClickToggleSideBar={handleOnClickToggleSideBar}
+              headerName={headerName}
+              isSideBarOpen={isSideBarOpen}
+            />
+            <div className={styles.Content_Main}>
+              <Component />
             </div>
           </div>
-        )}
-      />
-    ) : null;
-  };
-
-  //  TODO refactor this function to fit with WorkRoute
-  const renderUserProfilePage = () => {
-    return userDatabase.map((user) => (
-      <Route
-        exact
-        path={`/userprofile=${user.uniqueID}`}
-        key={`userprofile-${user.uniqueID}`}
-        render={(props) => {
-          return (
-            <div className={styles.WorkSpace} style={{ height: "100vh" }}>
-              {isSideBarOpen ? (
-                <SideBar handleOnClick={handleOnClickToggleSideBar} />
-              ) : null}
-              <div className={styles.WorkSpace_Content}>
-                <WorkspaceNavigationBar
-                  handleOnClickToggleSideBar={handleOnClickToggleSideBar}
-                  isSideBarOpen={isSideBarOpen}
-                  headerName={"User Profile"}
-                />
-                <div className={styles.Content_Main}>
-                  <UserProfilePage props={props} userAccount={user} />
-                </div>
-              </div>
-            </div>
-          );
-        }}
-      />
-    ));
-  };
-
-  const renderProjectPages = () => {
-    return projectDatabase.map((project) => (
-      <Route
-        exact
-        path={`/projects/project=${formalizeProjectName(project.projectName)}`}
-        key={`project-${formalizeProjectName(project.projectName)}`}
-        render={(props) => {
-          return (
-            <div className={styles.WorkSpace}>
-              {isSideBarOpen ? (
-                <SideBar handleOnClick={handleOnClickToggleSideBar} />
-              ) : null}
-              <div className={styles.WorkSpace_Content}>
-                <WorkspaceNavigationBar
-                  handleOnClickToggleSideBar={handleOnClickToggleSideBar}
-                  isSideBarOpen={isSideBarOpen}
-                  isProject
-                  headerName={`Project: ${project.projectName}`}
-                />
-                <div className={styles.Content_Main}>
-                  <ProjectPage props={props} project={project} />
-                </div>
-              </div>
-            </div>
-          );
-        }}
-      />
-    ));
+        </div>
+      </Route>
+    );
   };
 
   return (
@@ -117,26 +62,14 @@ const App = ({ userDatabase, projectDatabase, currentUser }) => {
         <Route path="/registration" exact component={RegisterPage} />
         <Route path="/login" exact component={LoginPage} />
         <Route path="/projects/project=new" exact component={ProjectNew} />
-        <WorkRoute
-          path="/homepage"
-          exact
-          Component={HomePage}
-          headerName="Homespace"
-        />
-        <WorkRoute
-          path="/projects"
-          exact
-          Component={ProjectOverviewPage}
-          headerName="Projects"
-        />
-        <WorkRoute
-          path="/tasks"
-          exact
-          Component={TaskPage}
-          headerName="My Tasks"
-        />
-        {renderUserProfilePage()}
-        {renderProjectPages()}
+        {routeNavigationLayout("/homepage", true, "Homespace", HomePage)}
+        {routeNavigationLayout(
+          "/projects",
+          true,
+          "Projects",
+          ProjectOverviewPage
+        )}
+        {routeNavigationLayout("/tasks", true, "My Tasks", TaskPage)}
         <Route component={Error404NotFoundPage} />
       </Switch>
     </BrowserRouter>
