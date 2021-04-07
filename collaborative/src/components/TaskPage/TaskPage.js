b59b8c27caf6d774a52053915f17ff4228d74604
Programmernammer <ProgrammerNammer@users.noreply.github.com>
2021-04-07 16:33:26 +0800

    Refactored Edit Task and Add Task forms

--- collaborative/src/components/TaskPage/TaskPage.js
+++ collaborative/src/components/TaskPage/TaskPage.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
-import Form from "../Form/Form";
+import { Formik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
 import Popup from "reactjs-popup";
+import * as Yup from "yup";
+import styles from "./TaskPage.module.css";
+
+import {
+  Division,
+  FormDesign,
+  FieldWithError,
+  RowDivision,
+} from "../FormDesign/FormDesign";
 import Button from "../Button/Button";
 import CloseButton from "../../assets/CloseButton.svg";
 import New__White from "../../assets/New__White.svg";
 import TaskPreviewList from "../TaskPreviewList/TaskPreviewList";
-import styles from "./TaskPage.module.css";
-import { isOverflowingVertically } from "../../logic";
-import { Link } from "react-router-dom";
+
+const taskSchema = Yup.object().shape({
+  taskName: Yup.string()
+    .required("Task must have a name")
+    .min(5, "Task name is too short - at least 5 characters.")
+    .max(20, "Task name is too long - at most 20 characters."),
+  taskDescription: Yup.string(),
+  taskPriority: Yup.number(),
+});
 
 // TODO Fix misalignment styling whenever there there is no more scroll
 const TaskPage = () => {
@@ -24,48 +40,62 @@ const TaskPage = () => {
         position="center center"
         onClose={() => setAddTaskModalStatus(false)}
       >
-        <Form
-          width="600px"
-          formPurpose="addTask"
-          handleOnSubmitCustomized={() => setAddTaskModalStatus(false)}
-        >
-          <img
-            src={CloseButton}
-            alt="Close Button"
-            handleOnClick={() => setAddTaskModalStatus(false)}
-            clickable
-            flex__end
-          />
-          <label for="taskNameInput">
-            <h1 style={{ textTransform: "capitalize" }}>Create a New Task</h1>
-          </label>
-          <div Column>
-            <input
-              type="text"
-              name="task-name-input"
-              id="taskNameInput"
-              placeholder="Task Name"
-              required
-              editableText
-            />
-            <div Column>
-              <label for="descriptionInput">Task Description</label>
-              <textarea
-                id="descriptionInput"
-                placeholder="My utmost important task description."
-              />
-            </div>
-            <div>
-              <label for="priorities">Priority</label>
-              <select id="priorities" name="priorityInput" required>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-              </select>
-            </div>
-          </div>
-          <input type="submit" value="Create Task" />
-        </Form>
+        <FormDesign width="500px" className={styles.TaskPage_Form}>
+          <Formik
+            initialValues={{
+              // TODO inject what the task's data in initialValues
+              taskName: "",
+              taskDescription: "",
+              taskPriority: 1,
+            }}
+            validationSchema={taskSchema}
+            onSubmit={(formData) => {
+              const data = JSON.stringify(formData, null, 2);
+              setAddTaskModalStatus(false);
+            }}
+          >
+            <Form>
+              <Division>
+                <img
+                  src={CloseButton}
+                  alt="Close Button"
+                  onClick={() => setAddTaskModalStatus(false)}
+                  style={{
+                    alignSelf: "flex-end",
+                    cursor: "pointer",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                />
+                <Division gap="20px">
+                  <h1>Create a New Task</h1>
+                  <FieldWithError
+                    name="taskName"
+                    type="text"
+                    placeHolder="Task Name"
+                  />
+                  <Division gap="10px">
+                    <label>Task Description</label>
+                    <Field
+                      name="taskDescription"
+                      as="textarea"
+                      placeHolder="Your Task Description"
+                    />
+                  </Division>
+                  <RowDivision>
+                    <label for="priorities">Priority</label>
+                    <Field name="priorities" as="select">
+                      <option value="low">Low</option>
+                      <option value="medium">Medium</option>
+                      <option value="high">High</option>
+                    </Field>
+                  </RowDivision>
+                  <Field type="submit" value="Create New Task" />
+                </Division>
+              </Division>
+            </Form>
+          </Formik>
+        </FormDesign>
       </Popup>
     );
   };
@@ -80,49 +110,62 @@ const TaskPage = () => {
         position="center center"
         onClose={() => setEditTaskModalStatus(false)}
       >
-        <Form
-          width="600px"
-          formPurpose="addTask"
-          linkTo="/login"
-          handleOnSubmitCustomized={() => setEditTaskModalStatus(false)}
-        >
-          <img
-            src={CloseButton}
-            alt="Close Button"
-            handleOnClick={() => setEditTaskModalStatus(false)}
-            clickable
-            flex__end
-          />
-          <label for="taskNameInput">
-            <h1 style={{ textTransform: "capitalize" }}>Edit Task</h1>
-          </label>
-          <div Column>
-            <input
-              type="text"
-              name="task-name-input"
-              id="taskNameInput"
-              placeholder="Task Name"
-              required
-              editableText
-            />
-            <div Column>
-              <label for="descriptionInput">Task Description</label>
-              <textarea
-                id="descriptionInput"
-                placeholder="My utmost important task description."
-              />
-            </div>
-            <div>
-              <label for="priorities">Priority</label>
-              <select id="priorities" name="priorityInput" required>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-              </select>
-            </div>
-          </div>
-          <input type="submit" value="Create Task" />
-        </Form>
+        <FormDesign width="500px" className={styles.TaskPage_Form}>
+          <Formik
+            initialValues={{
+              // TODO inject what the task's data in initialValues
+              taskName: "",
+              taskDescription: "",
+              taskPriority: 1,
+            }}
+            validationSchema={taskSchema}
+            onSubmit={(formData) => {
+              const data = JSON.stringify(formData, null, 2);
+              setEditTaskModalStatus(false);
+            }}
+          >
+            <Form>
+              <Division>
+                <img
+                  src={CloseButton}
+                  alt="Close Button"
+                  onClick={() => setEditTaskModalStatus(false)}
+                  style={{
+                    alignSelf: "flex-end",
+                    cursor: "pointer",
+                    width: "30px",
+                    height: "30px",
+                  }}
+                />
+                <Division gap="20px">
+                  <h1>Edit Task</h1>
+                  <FieldWithError
+                    name="taskName"
+                    type="text"
+                    placeHolder="Task Name"
+                  />
+                  <Division gap="10px">
+                    <label>Task Description</label>
+                    <Field
+                      name="taskDescription"
+                      as="textarea"
+                      placeHolder="Your Task Description"
+                    />
+                  </Division>
+                  <RowDivision>
+                    <label for="priorities">Priority</label>
+                    <Field name="priorities" as="select">
+                      <option value="low">Low</option>
+                      <option value="medium">Medium</option>
+                      <option value="high">High</option>
+                    </Field>
+                  </RowDivision>
+                  <Field type="submit" value="Save Edited Task" />
+                </Division>
+              </Division>
+            </Form>
+          </Formik>
+        </FormDesign>
       </Popup>
     );
   };
