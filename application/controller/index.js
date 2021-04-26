d594c0c8258620f0938ea7ce34c04e80e6cd0a5f
Programmernammer <ProgrammerNammer@users.noreply.github.com>
2021-04-26 15:04:42 +0800

    Formatting

--- application/controller/index.js
+++ application/controller/index.js
@@ -1,25 +1,17 @@
-const dotenv = require('dotenv');
-const fs = require('fs');
-const express = require('express');
-const db = require('./models/db.js');
+const dotenv = require("dotenv");
+const fs = require("fs");
+const express = require("express");
+const db = require("./models/db.js");
 
-const app = express(); // Initialize Express Server 
+const app = express(); // Initialize Express Server
 
-dotenv.config(); 
+dotenv.config();
 
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
-
-
-
-
-
-
-
-
 // Bounding Server (Essentially checking if the server is listening (working))
-app.listen(port , hostname , function(){
-    console.log('Server Running at:');
-    console.log('http://' + hostname + ':' + port);
+app.listen(port, hostname, function () {
+  console.log("Server Running at:");
+  console.log("http://" + hostname + ":" + port);
 });
