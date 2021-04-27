b401bcbfac26b30279f57a7a8f93cebe26aea0d6
Programmernammer <ProgrammerNammer@users.noreply.github.com>
2021-04-27 13:02:49 +0800

    Registration POST request added

--- application/controller/index.js
+++ application/controller/index.js
@@ -1,7 +1,7 @@
-const dotenv = require("dotenv");
-const fs = require("fs");
-const express = require("express");
 const db = require("./models/db.js");
+const dotenv = require("dotenv");
+const express = require("express");
+const fs = require("fs");
 
 const app = express(); // Initialize Express Server
 
@@ -9,6 +9,10 @@ dotenv.config();
 
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
+
+app.post("/api/registerUser", (req, res) => {
+  console.table(req.query);
+});
 
 // Bounding Server (Essentially checking if the server is listening (working))
 app.listen(port, hostname, function () {
