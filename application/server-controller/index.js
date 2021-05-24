eab454a06ebe32ee29def6bee11be9de9ba1fcd5
Jerickson <jerickson_lee@dlsu.edu.ph>
2021-05-24 13:39:21 +0800

    Bug Fix for Heroku

--- application/server-controller/index.js
+++ application/server-controller/index.js
@@ -9,8 +9,8 @@ const app = express();
 
 dotenv.config();
 
-port = process.env.PORT || 5000;
-hostname = process.env.HOSTNAME;
+port = process.env.PORT || 3000;
+hostname = process.env.HOSTNAME || 0.0.0.0;
 
 //  TODO add cors() for extra security
 // app.use(cors());
